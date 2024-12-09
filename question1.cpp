656c430a1d98aafca84801dfc495d4a359b5f387
FV101LW <limweim48@gmail.com>
2024-12-10 00:49:30 +0800

    Update question1.cpp

--- question1.cpp
+++ question1.cpp
@@ -14,3 +14,20 @@ vector<vector<int>> adjacencyList (int V, const vector<pair<int, int>>& edges) {
   }
   return adList;
 }
+
+void printList(const vector<vector<int>>& adList) {
+  for (int i = 0; i < adList.size(); i++) {
+    cout << i << ": ";
+    for (int neighbor : adList[i]) {
+      cout << neighbor << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  int v = 5;
+  vector<pair<int, int>> edges = {{0,1}, {0,4}, {1,2}, {1,3}, {3,4}};
+
+  vector<vector<int>> adList = adjacencyList (V, edges);
+}
