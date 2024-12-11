0429054c4f279725aa3a59210cde8125ebd2a9e9
FV101LW <limweim48@gmail.com>
2024-12-11 20:36:02 +0800

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -9,12 +9,16 @@
 using namespace std;
 
 vector<int> traversal (const vector<vector<int>>& adj) {
+  // Initialize vector for storage of BFS traversal
   vector<int> result;
 
+  // Keeps track of nodes already visited
   vector<bool> visited(adj.size(), false);
 
+  // Queue for nodes in BFS order
   queue<int> q;
 
+  // BFS start from vertex 0
   q.push(0);
   visited[0] = true;
 
@@ -23,7 +27,8 @@ vector<int> traversal (const vector<vector<int>>& adj) {
     q.pop();
     result.push_back(node);
 
-    //nb = neighbor
+    // nb = neighbor
+    // Traverse adjacents nodes of current node
     for (int nb : adj[node]) {
       if (!visited[nb]) {
         visited[nb] = true;
@@ -37,8 +42,10 @@ vector<int> traversal (const vector<vector<int>>& adj) {
 int main() {
   vector<vector<int>> adj = {{2, 3, 1}, {0}, {0,4}, {0}, {2}};
 
+  //Conducts BFS Traversal || resBFS = result of BFS
   vector<int> resBFS = traversal(adj);
 
+  //Output
   cout << "BFS Traversal: ";
   for (int node: resBFS) {
     cout << node << " ";
