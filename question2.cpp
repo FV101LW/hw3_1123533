f4428eb396e080dcfc6ea20666e3aa78b3445a85
FV101LW <limweim48@gmail.com>
2024-12-11 20:25:18 +0800

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -10,4 +10,39 @@ using namespace std;
 
 vector<int> traversal (const vector<vector<int>>& adj) {
   vector<int> result;
+
+  vector<bool> visited(adj.size(), false);
+
+  queue<int> q;
+
+  q.push(0);
+  visited[0] = true;
+
+  while (!q.empty()) {
+    int node = q.front();
+    q.pop();
+    result.push_back(node);
+
+    //nb = neighbor
+    for (int nb : adj[node]) {
+      if (!visited[nb]) {
+        visited[nb] = true;
+        q.push(nb);
+      }
+    }
+  }
+  return result;
+}
+
+int main() {
+  vector<vector<int>> adj = {{2, 3, 1}, {0}, {0,4}, {0}, {2}};
+
+  vector<int> resBFS = traversal(adj);
+
+  cout << "BFS Traversal: ";
+  for (int node: resBFS) {
+    cout << node << " ";
+  }
+  cout << endl;
+  return 0;
 }
