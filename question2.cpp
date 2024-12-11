4b7d5e8d597e5af769d2f0c775a28bf4f8732681
FV101LW <limweim48@gmail.com>
2024-12-12 00:23:04 +0800

    Update question2.cpp

--- question2.cpp
+++ question2.cpp
@@ -8,48 +8,48 @@
 #include <queue>
 using namespace std;
 
-vector<int> traversal (const vector<vector<int>>& adj) {
-  // Initialize vector for storage of BFS traversal
-  vector<int> result;
+vector<int> traversal(const vector<vector<int>>& adj) {
+    // Initialize vector for storage of BFS traversal
+    vector<int> result;
 
-  // Keeps track of nodes already visited
-  vector<bool> visited(adj.size(), false);
+    // Keeps track of nodes already visited
+    vector<bool> visited(adj.size(), false);
 
-  // Queue for nodes in BFS order
-  queue<int> q;
+    // Queue for nodes in BFS order
+    queue<int> q;
 
-  // BFS start from vertex 0
-  q.push(0);
-  visited[0] = true;
+    // BFS start from vertex 0
+    q.push(0);
+    visited[0] = true;
 
-  while (!q.empty()) {
-    int node = q.front();
-    q.pop();
-    result.push_back(node);
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        result.push_back(node);
 
-    // nb = neighbor
-    // Traverse adjacents nodes of current node
-    for (int nb : adj[node]) {
-      if (!visited[nb]) {
-        visited[nb] = true;
-        q.push(nb);
-      }
+        // nb = neighbor
+        // Traverse adjacents nodes of current node
+        for (int nb : adj[node]) {
+            if (!visited[nb]) {
+                visited[nb] = true;
+                q.push(nb);
+            }
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 int main() {
-  vector<vector<int>> adj = {{2, 3, 1}, {0}, {0,4}, {0}, {2}};
+    vector<vector<int>> adj = { {1,3}, {0,2}, {1,3}, {0,2} };
 
-  //Conducts BFS Traversal || resBFS = result of BFS
-  vector<int> resBFS = traversal(adj);
+    //Conducts BFS Traversal || resBFS = result of BFS
+    vector<int> resBFS = traversal(adj);
 
-  //Output
-  cout << "BFS Traversal: ";
-  for (int node: resBFS) {
-    cout << node << " ";
-  }
-  cout << endl;
-  return 0;
+    //Output
+    cout << "BFS Traversal: ";
+    for (int node : resBFS) {
+        cout << node << " ";
+    }
+    cout << endl;
+    return 0;
 }
