c212601725ee312f41f7a51167259023edce7135
FV101LW <limweim48@gmail.com>
2024-12-11 16:40:04 +0800

    Create question2.cpp

---
+++ question2.cpp
@@ -0,0 +1,13 @@
+// Homework 3 - Question 2
+// Wei-li Lin
+// 1123533
+// 12-11-2024
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> traversal (const vector<vector<int>>& adj) {
+  vector<int> result;
+}
